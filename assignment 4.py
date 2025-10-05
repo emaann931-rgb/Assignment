0ea62f6799cccb711e3c05f412955be97d6ed760
Noor13456 <noorgrps@gmail.com>
2025-10-05 16:18:15 +0500

    1

--- assignment 4.py
+++ assignment 4.py
@@ -1,3 +1,30 @@
+#convert the following into boolean 
 a="True"
-b=""
+b=bool(a) 
+print(b)
 print(type(b))
+
+c=bool("False")
+print(c)
+print(type(c))
+
+d=bool("0")
+print(d)
+print(type(d))
+
+e=bool("1")
+print(e)
+print(type(e))
+
+#Convert a string to an integer
+integervalue=int(float("4.5869"))
+print(integervalue)
+
+
+#Convert to float
+floattrue=float(True)
+print(floattrue)
+floatfalse=float(False)
+print(floatfalse)
+floatPython=float(Python)
+print(floatPython)
